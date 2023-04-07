f523eab3cdd17e1aab116394863db4b249af6b21
AlyssaWillow <alyssawillow10@gmail.com>
2023-04-07 03:17:39 -0400

    rjwfljasldkfma

--- src/app/pipes/selected-game.pipe.ts
+++ src/app/pipes/selected-game.pipe.ts
@@ -1,5 +1,27 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { BoardGame } from '../models/collection';
+import { factionDb2, factionTypeData } from '../models/faction';
+import { nameId } from '../models/generic';
+
+@Pipe({
+  name: 'expansionGameFilter'
+})
+export class expansionPipe implements PipeTransform {
+  transform(values: BoardGame[], ...args: any[]): BoardGame[] {
+    return values
+      .filter(v => {
+        if (v !== null && v.objectid !== null) {
+          if (args[0] === null) {
+            return true;
+          } else {
+              return (args.includes(v.objectid));
+          }
+        } else {
+          return false;
+        }
+      })
+  }
+}
 
 @Pipe({
   name: 'selectedGameFilter'
@@ -11,11 +33,75 @@ export class selectedGamePipe implements PipeTransform {
         if (v !== null && v.objectid !== null) {
           if (args[0] === null) {
             return true;
-          } else if (v.objectid === args[0][0].objectid) {
+          } else if (v.objectid === args[0][0]?.objectid) {
             return true
           } else {
-              let expIds: string[] = args[0][1].map((a: { objectid: BoardGame; }) => a.objectid);
-              return (expIds.includes(v.objectid));
+              let expIds: string[] = args[0][1]?.map((a: { objectid: BoardGame; }) => a.objectid);
+              return (expIds?.includes(v.objectid));
+          }
+        } else {
+          return false;
+        }
+      })
+  }
+}
+
+@Pipe({
+  name: 'baseGameFilter'
+})
+export class basePipe implements PipeTransform {
+  transform(values: BoardGame[], ...args: any[]): BoardGame[] {
+    return values
+      .filter(v => {
+        if (v !== null && v.objectid !== null) {
+          if (args[0] === null) {
+            return true;
+          } else {
+              return (!args.includes(v.objectid));
+          }
+        } else {
+          return false;
+        }
+      })
+  }
+}
+
+@Pipe({
+  name: 'specificExpansionFilter'
+})
+export class specificExpansionsPipe implements PipeTransform {
+  transform(values: BoardGame[], ...args: any[]): BoardGame[] {
+    return values
+      .filter(v => {
+        if (v !== null && v.objectid !== null) {
+          if (args[0] === null) {
+            return true;
+          } else {
+              return args[0][1].get(args[0][0])?.includes(v.objectid);
+          }
+        } else {
+          return false;
+        }
+      })
+  }
+}
+
+@Pipe({
+  name: 'factionTypesFilter'
+})
+export class factionTypesPipe implements PipeTransform {
+  transform(values: nameId[], ...args: any[]): nameId[] {
+    return values
+      .filter(v => {
+        if (v !== null) {
+          if (args[0] === null) {
+            return true;
+          } else {
+            return args[0][0]
+              .filter((f: factionDb2) => f.gameId === args[0][1])
+              .map((m: any)=>m.typeId)
+              .filter((value: any, index: any, self: string | any[]) => self.indexOf(value) === index)
+              .includes(v.id);
           }
         } else {
           return false;
